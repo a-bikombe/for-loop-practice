00b96b5f476e00298d7167d0f1c6ff338b288ce7
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-19 18:28:57 -0400

    init

---
+++ ForLoopPractice/ForLoopPractice.java
@@ -0,0 +1,10 @@
+package ForLoopPractice;
+
+public class ForLoopPractice {
+
+    public static void main(String[] args) {
+	for (int number = 1; number <= 10; number++) {
+            System.out.println(number + " ");
+	}
+    }
+}
